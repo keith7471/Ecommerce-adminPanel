e7ae4f464ae7fe41cb3bdf8acf8ff2d1b2207114
keith7471 <93572477+keith7471@users.noreply.github.com>
2025-03-16 22:32:48 +0530

    2

--- Server/src/db.js
+++ Server/src/db.js
@@ -5,10 +5,10 @@ env.config()
 
 const db = new pg.Client({
     user: process.env.PG_USER,
-    host: process.env.PG_HOST || 'localhost', // Wrap localhost in quotes
+    host: process.env.PG_HOST || 'localhost', 
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT || 5432, // Default to PostgreSQL port
+    port: process.env.PG_PORT || 5432, 
 });
 
 db.connect().then(() => console.log('Connected to PostgresSQL')).catch((err) => console.log('Database connection error',err));
